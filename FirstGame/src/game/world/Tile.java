48458fba91ba04870e4b7d631436fadd773e6dea
InterdimensionalCat <bennett.thomas99@gmail.com>
2018-02-25 13:02:58 -0500

    Finished different tile types

--- FirstGame/src/game/world/Tile.java
+++ FirstGame/src/game/world/Tile.java
@@ -20,8 +20,12 @@ public class Tile {
 	public static final Tile tile1 = new Tile(0xFF000000, new Texture(terrain, 3, 1 , 64 , 64), TileType.SOLID);
 	public static final Tile tile2 = new Tile(0xFFFF0000, new Texture(terrain, 2, 1 , 64, 64), TileType.SOLID /*TileType.SLOPE_RIGHT_64_00*/);
 	public static final Tile tile3 = new Tile(0xFFFFFFFF, new Texture(terrain, 4, 1 , 64, 64), TileType.SOLID);
-	public static final Tile tile4 = new Tile(0xFF0000FF, new Texture(terrain, 1, 2 , 64, 64), TileType.SOLID);
+	public static final Tile tile4 = new Tile(0xFF00FFFF, new Texture(terrain, 1, 2 , 64, 64), TileType.SOLID);
 	public static final Tile tile5 = new Tile(0xFF00FF00, new Texture(terrain, 2, 2 , 64, 64), TileType.SOLID);
+	public static final Tile tile6 = new Tile(0xFFF0F0F0, new Texture(terrain, 3, 2 , 64, 64), TileType.SOLID);
+	public static final Tile tile7 = new Tile(0xFF0F0F0F, new Texture(terrain, 4, 2 , 64, 64), TileType.SOLID);
+	public static final Tile tile8 = new Tile(0xFF111111, new Texture(terrain, 1, 3 , 64, 64), TileType.SOLID);
+	public static final Tile tile9 = new Tile(0xFF222222, new Texture(terrain, 2, 3 , 64, 64), TileType.SOLID);
 	
 	private Tile(int id, Texture sprite, TileType type) {
 		this.id = id;
